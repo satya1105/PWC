c8d198b634b01523c6c47f07a5936e3eec494c15
satya ranjan <satya.ranjan.robomq@gmail.com>
2024-12-05 22:19:48 +0530

    add services

---
+++ country-dashboard-frontend/src/app/services/api.tsx
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3001', // Backend API URL
+});
+
+export const getCountries = () => api.get('/countries');
+export const getCountryDetails = (code: string) => api.get(`/countries/${code}`);
