b96d5c449946c4b680313e8e2aff45deb2b061ef
satya ranjan <satya.ranjan.robomq@gmail.com>
2024-12-08 18:06:19 +0530

    add vercel

--- country-dashboard-frontend/next.config.ts
+++ country-dashboard-frontend/next.config.ts
@@ -1,7 +1,16 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // Enable strict mode for better debugging
+  distDir: ".next", // Specify the build directory (default is .next)
+  output: "standalone", // Enable standalone mode for deployment
 };
 
 export default nextConfig;
