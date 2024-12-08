56a604a751c84aa3c2e9fe7192185d3485bc1039
satya ranjan <satya.ranjan.robomq@gmail.com>
2024-12-08 22:53:51 +0530

    remove unused code

--- country-dashboard-frontend/src/components/SearchBar.tsx
+++ country-dashboard-frontend/src/components/SearchBar.tsx
@@ -1,26 +1,3 @@
-// import React from 'react';
-
-// interface SearchBarProps {
-//   onSearch: (query: string) => void;
-// }
-
-// const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
-//   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     onSearch(e.target.value);
-//   };
-
-//   return (
-//     <input
-//       type="text"
-//       placeholder="Search countries..."
-//       className="p-2 border rounded w-full"
-//       onChange={handleSearch}
-//     />
-//   );
-// };
-
-// export default SearchBar;
-
 import React from 'react';
 
 interface SearchBarProps {
